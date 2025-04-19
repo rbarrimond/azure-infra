4448b0a35592d2ef527091fae495657992e0a9ed
Robert Barrimond <robert@barrimond.com>
2025-04-18 23:59:23 -0400

    Implement Azure backend configuration in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,8 @@
-// backend.tf placeholder
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "base"
+    storage_account_name = "sabarrimond01"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  }
+}
