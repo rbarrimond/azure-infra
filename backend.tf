b0a3cfa67bb28077a5ac5654422aaf664bb4eb45
Robert Barrimond <robert@barrimond.com>
2025-04-19 22:43:31 -0400

    Update backend.tf to change the state file key to 'core/terraform.tfstate'

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "base"
     storage_account_name = "sabarrimond01"
     container_name       = "terraform"
-    key                  = "terraform.tfstate"
+    key                  = "core/terraform.tfstate"
   }
 }
