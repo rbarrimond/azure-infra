e2d4cb11c65df35e384e3724802fb15dcea4d80d
Robert Barrimond <robert@barrimond.com>
2025-06-02 11:57:27 -0400

    Update Python version in the_rob_vault function app from 3.11 to 3.10

--- modules/shared_ai/main.tf
+++
@@ -1,6 +0,0 @@
-// shared_ai module
-resource "null_resource" "shared_ai_example" {
-  provisioner "local-exec" {
-    command = "echo shared_ai deployed"
-  }
-}
--- modules/the_rob_vault/main.tf
+++ modules/the_rob_vault/main.tf
@@ -51,7 +51,7 @@ resource "azurerm_linux_function_app" "the_rob_vault" {
     application_insights_key = var.application_insights_key
 
     application_stack {
-      python_version = "3.11"
+      python_version = "3.10"
     }
   }
 
