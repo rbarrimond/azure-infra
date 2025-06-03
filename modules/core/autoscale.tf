9795a34866f2c4f986c0a7fc9570f57d8a96892e
Robert Barrimond <robert@barrimond.com>
2025-06-03 02:40:10 -0400

    Add Azure Monitor Autoscale configuration for App Service Plan

---
+++ modules/core/autoscale.tf
@@ -0,0 +1,57 @@
+# Azure Monitor Autoscale for App Service Plan
+resource "azurerm_monitor_autoscale_setting" "core_service_plan" {
+  name                = "autoscale-asp-${var.suffix}"
+  resource_group_name = azurerm_resource_group.core.name
+  location            = azurerm_resource_group.core.location
+  target_resource_id  = azurerm_service_plan.core.id
+  enabled             = true
+
+  profile {
+    name = "default"
+    capacity {
+      minimum = "1"
+      maximum = "3"
+      default = "1"
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_service_plan.core.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 70
+      }
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_service_plan.core.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 30
+      }
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+  }
+
+  tags = var.default_tags
+}
